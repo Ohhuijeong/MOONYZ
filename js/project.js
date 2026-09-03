c6400f498b4eaeca877c1ccb534a3aea376d9f91
오희정 <okclass5@gmail.com>
2026-09-03 18:10:04 +0900

    프로젝트 진행 중1

---
+++ js/project.js
@@ -0,0 +1,9 @@
+var projectSwiper = new Swiper('.projectSwiper', {
+  slidesPerView: 'auto',
+  centeredSlides: true,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
