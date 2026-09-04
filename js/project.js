6b56376332d003585d5c8a626b1a5b6b5df80721
오희정 <okclass5@gmail.com>
2026-09-04 17:31:31 +0900

    project 페이지 완료

--- js/project.js
+++ js/project.js
@@ -9,7 +9,31 @@ var projectSwiper = new Swiper('.projectSwiper', {
 });
 
 $(function () {
-  $('.detail-btn').on('click', function() {
-    
+  $('.detail-btn').on('click', function () {
+    $('#detail-board-area').addClass('show');
+
+    const project = $(this).data('project');
+
+    if (project === 'baseball') {
+      $('.baseball-board').addClass('show');
+    } else if (project === 'motors') {
+      $('.motors-board').addClass('show');
+    } else {
+      $('.museum-board').addClass('show');
+    }
+
+    $('#detail-board-area').on('click', function (e) {
+      if ($(e.target).closest('.detail-board-frame').length) {
+        return;
+      } else {
+        $(this).removeClass('show');
+        $('.detail-board-frame').removeClass('show')
+      }
+    })
+
+    $('.xcircle').on('click', function () {
+      $('#detail-board-area').removeClass('show');
+      $('.detail-board-frame').removeClass('show');
+    })
   })
 })
