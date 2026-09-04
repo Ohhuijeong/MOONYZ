2baa12ef0c3abc27a10f4e60c6c44346b83d9aca
김준희 <projecteam7389@gmail.com>
2026-09-04 16:25:03 +0900

    고객센터 최종 수정3

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,10 @@
+/* 창 열리기 */
 $(".question-card").mouseenter(function () {
     $(".answer").slideUp();
     $(this).css("height", "auto");
     $(this).find(".answer").slideToggle();
 });
-
+/* 창 닫히기 */
 $(".question-card").mouseleave(function () {
     $(".answer").slideUp();
     $(this).animate({
